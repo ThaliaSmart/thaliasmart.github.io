faafd558605dfe6b975d93500f13ab9eadf6db2c
ThaliaSmart <thaliasmart326@aol.com>
2016-08-19 13:49:08 -0400

    added the first 55 files
    going to see if it looks good on phone

---
+++ Hair.js
@@ -0,0 +1,48 @@
+<div data-role="page" id="pageone">
+
+ <div data-role="main" class="ui-content">
+
+   <a href="#pagetwo">Go to Page Two</a>
+
+ </div>
+
+</div>
+
+<div data-role="page" id="pagetwo">
+
+ <div data-role="main" class="ui-content">
+
+   <a href="#pageone">Go to Page One</a>
+
+ </div>
+</div>
+
+function myFunction1() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+function myFunction(){
+		
+	<a href="http://www.forever21.com"></a>
+		
+	var words = document.getElementById("welcome");
+	words.innerHTML = "Natural girls rule";
+	var /flower = document.getElementById("flower");
+			
+	if(flower.src.match("flower.jpg")){
+		flower.src = "http://www.forever21.com";
+				
+	}else(
+		words.innerHTML ="litt";
+		alien.src = "flower.jpg";
+				
+			
+}
+
+function myFunction5(){
+	window.open("http://www.forever21.com")
+}
